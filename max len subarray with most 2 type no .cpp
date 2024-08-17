1d4aeaeb5dc99f9d01352ea25840eb2ae93810c3
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2024-08-17 14:04:25 +0530

    Update max len subarray with most 2 type no .cpp

--- max len subarray with most 2 type no .cpp
+++ max len subarray with most 2 type no .cpp
@@ -3,11 +3,14 @@ int l=0,r=0;
     map<int,int>m;
     while(r<n){
         m[v[r]]++;
-        while(m.size()>k){
+        if(m.size()>k){
             m[v[l]]--;
                 if(m[v[l]]==0)m.erase(v[l]);
                 l++;
             }
+        if(m.size()<=2){
             maxlen=max(maxlen,r-l+1);
+        }
             r++;
     }
+return maxlen;
