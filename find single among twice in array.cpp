68050e733dd8527796f6ee9c43ec5820dc23ef43
22himanshuvashishtha <149816813+22himanshuvashishtha@users.noreply.github.com>
2024-08-29 08:37:16 +0530

    Create find single among twice in array.cpp

---
+++ find single among twice in array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l=1,r=nums.size()-2,mid=0,count=0;
+        if(nums.size()==1)return nums[0];
+        if(nums[l]!=nums[0])return nums[0];
+        if(nums[r]!=nums[r+1])return nums[r+1];
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(nums[mid-1]!=nums[mid] && nums[mid+1]!=nums[mid]){
+                return nums[mid];
+            }
+            if((mid%2!=0 && nums[mid-1]==nums[mid])||(mid%2==0 && nums[mid]==nums[mid+1])){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
